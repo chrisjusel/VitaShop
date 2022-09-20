da4e6b6266ff9ebd1d7b1d3b4ea5f01d04367b60
chrisjusel <christian00.vitale@gmail.com>
2022-09-20 21:32:53 +0200

    maven project initialized

---
+++ src/main/java/it/vitashop/VitaShopApplication.java
@@ -0,0 +1,13 @@
+package it.vitashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/vitashop/VitaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vitashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
